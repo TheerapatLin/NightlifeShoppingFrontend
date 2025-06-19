59267a1224b434d7405684f265aed9efae97629b
Hiddengemtechno <hiddengemtechno@gmail.com>
2025-06-19 19:27:52 +0700

    update1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -73,7 +73,7 @@ function Profile() {
     <div className="flex flex-col gap-2 mt-[65px] w-full">
       <div className="flex justify-center">
         <div className="mt-2 flex items-center bg-gray-800 p-2 overflow-x-auto rounded-full space-x-2">
-           <button
+          <button
             className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
               selectedTab === "profile"
                 ? "bg-blue-500 text-white"
@@ -103,7 +103,7 @@ function Profile() {
           >
             Events
           </button>
-         
+
           <button
             className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
               selectedTab === "affiliate"
@@ -114,6 +114,18 @@ function Profile() {
           >
             Affiliate
           </button>
+          {(user?.role == "admin" || user?.role == "host") && (
+            <button
+              className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
+                selectedTab === "scheduler"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+              }`}
+              onClick={() => setSelectedTab("scheduler")}
+            >
+              Scheduler
+            </button>
+          )}
         </div>
       </div>
 
@@ -125,6 +137,27 @@ function Profile() {
         {selectedTab === "profile" && <UserProfile />}
 
         {selectedTab === "affiliate" && <AffiliateLinks />}
+
+        {selectedTab === "scheduler" && (user?.role == "admin" || user?.role == "host")  && (
+          <>
+            <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-2 w-full">
+              <div className="w-full lg:w-[70%]">
+                <CalendarSchedule
+                  onDateSelect={handleDateSelect}
+                  events={events}
+                  onEventClick={handleEventClick}
+                />
+              </div>
+              <div className="w-full lg:w-[30%] bg-white rounded-lg p-2 mt-2 lg:mt-0">
+                <ActivitiesForm
+                  selectedDate={selectedDate}
+                  selectedEvent={selectedEvent}
+                  onClose={handleCloseForm}
+                />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
