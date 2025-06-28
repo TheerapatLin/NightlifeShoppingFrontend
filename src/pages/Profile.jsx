2f4de643c89de72f3625161eb7e1c4a4575d4461
Hiddengemtechno <hiddengemtechno@gmail.com>
2025-06-28 21:56:00 +0700

    update1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -103,17 +103,18 @@ function Profile() {
           >
             Events
           </button>
-
-          <button
-            className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
-              selectedTab === "affiliate"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-700 text-gray-300 hover:bg-gray-600"
-            }`}
-            onClick={() => setSelectedTab("affiliate")}
-          >
-            Affiliate
-          </button>
+          {(user?.role == "admin" || user?.role == "affiliator_host" || user?.role == "host_affiliator" || user?.role == "affiliator") && (
+            <button
+              className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
+                selectedTab === "affiliate"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+              }`}
+              onClick={() => setSelectedTab("affiliate")}
+            >
+              Affiliate
+            </button>
+          )}
           {(user?.role == "admin" || user?.role == "host") && (
             <button
               className={`flex-shrink-0 px-4 py-2 rounded-full text-sm ${
@@ -138,26 +139,27 @@ function Profile() {
 
         {selectedTab === "affiliate" && <AffiliateLinks />}
 
-        {selectedTab === "scheduler" && (user?.role == "admin" || user?.role == "host")  && (
-          <>
-            <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-2 w-full">
-              <div className="w-full lg:w-[70%]">
-                <CalendarSchedule
-                  onDateSelect={handleDateSelect}
-                  events={events}
-                  onEventClick={handleEventClick}
-                />
+        {selectedTab === "scheduler" &&
+          (user?.role == "admin" || user?.role == "host") && (
+            <>
+              <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-2 w-full">
+                <div className="w-full lg:w-[70%]">
+                  <CalendarSchedule
+                    onDateSelect={handleDateSelect}
+                    events={events}
+                    onEventClick={handleEventClick}
+                  />
+                </div>
+                <div className="w-full lg:w-[30%] bg-white rounded-lg p-2 mt-2 lg:mt-0">
+                  <ActivitiesForm
+                    selectedDate={selectedDate}
+                    selectedEvent={selectedEvent}
+                    onClose={handleCloseForm}
+                  />
+                </div>
               </div>
-              <div className="w-full lg:w-[30%] bg-white rounded-lg p-2 mt-2 lg:mt-0">
-                <ActivitiesForm
-                  selectedDate={selectedDate}
-                  selectedEvent={selectedEvent}
-                  onClose={handleCloseForm}
-                />
-              </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
       </div>
     </div>
   );
